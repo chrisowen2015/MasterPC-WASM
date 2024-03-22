5f23f2a5bd7b91a137bdaf5b7201f89e691ecbd0
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2024-03-22 13:14:09 -0700

    Removed unneeded files

--- Api/Data/MasterPcdbContext.cs
+++ Api/Data/MasterPcdbContext.cs
@@ -16,9 +16,7 @@ public partial class MasterPcdbContext : DbContext
 
     public virtual DbSet<Cpu> Cpus { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("Connection_String"));
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("Connection_String"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
