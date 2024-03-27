78ee278afde2b9300968362f3b78164280a06921
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2024-03-26 22:36:21 -0700

    Added MSAL Auth
    Added MSAL Auth, debating on if/how to hide adminID, clientID, tenantID from code before merging

--- MasterPC-WASM/Program.cs
+++ MasterPC-WASM/Program.cs
@@ -1,6 +1,5 @@
 using MasterPC_WASM.Services.CPU;
 using MasterPC_WASM;
-using MasterPC_WASM.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -11,6 +10,24 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+builder.Services.AddAuthorizationCore(options =>
+{
+    options.AddPolicy("superusers", policy =>
+    {
+        policy.RequireAssertion(context => context.User.HasClaim(c =>
+        {
+            return c.Type == "oid" && c.Value.Contains("2db79c27-24a9-4c74-b63c-9076b7d20d68");
+        }));
+    });
+});
 
 builder.Services.AddScoped<ICPUService, CPUServiceClient>();
 
