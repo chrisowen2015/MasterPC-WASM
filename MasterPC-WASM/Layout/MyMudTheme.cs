966c10aacf01d20a0b6ada9391217a3622d0e79d
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2024-03-31 15:39:01 -0700

    Added Cascading theme

---
+++ MasterPC-WASM/Layout/MyMudTheme.cs
@@ -0,0 +1,22 @@
+﻿using MudBlazor;
+
+public class MyMudTheme : MudTheme
+{
+    public MudTheme GetTheme()
+    {
+        var theme = new MudTheme()
+        {
+            Palette = new PaletteLight()
+            {
+                Primary = "#594AE2",
+                AppbarBackground = "#594AE2"
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = "#594AE2",
+                AppbarBackground = "#594AE2"
+            }
+        };
+        return theme;
+    }
+}
