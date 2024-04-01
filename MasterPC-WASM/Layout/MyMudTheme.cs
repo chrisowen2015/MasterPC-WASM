7cef06ac79282e2a71573d4f08767f55e1e614ee
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2024-03-31 23:22:01 -0700

    Added View Component Layout
    Added View Component Layout and created view product page for each component type

--- MasterPC-WASM/Layout/MyMudTheme.cs
+++ MasterPC-WASM/Layout/MyMudTheme.cs
@@ -9,7 +9,8 @@ public class MyMudTheme : MudTheme
             Palette = new PaletteLight()
             {
                 Primary = "#594AE2",
-                AppbarBackground = "#594AE2"
+                AppbarBackground = "#594AE2",
+                Background = Colors.Grey.Lighten4,
             },
             PaletteDark = new PaletteDark()
             {
